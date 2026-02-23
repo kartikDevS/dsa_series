416c9d7992bcb29ba6245ba473c56907997c06ce
kartikDevS <kartik9136@gmail.com>
2026-02-23 20:02:19 +0530

    practicing insertionSort

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,8 +2,34 @@
 //unsorted array so now we take first element from this unsorted array and place it at the right idx in
 // the sorted array 
 
+// #include <stdio.h>
+// void insertion(int arr[],int n){
+//     for(int i=1;i<n;i++){
+//         int curr=arr[i];
+//         int prev=i-1;
+//         while(prev>=0 && arr[prev]>curr){
+//             arr[prev+1]=arr[prev];
+//             prev--;
+//         }
+//         arr[prev+1]=curr;
+//     }
+// }
+
+// void main(){
+//     int arr[]={1,3,26,7,6,8,13,4};
+//     int n= sizeof(arr)/sizeof(arr[0]);
+//     insertion(arr,n);
+//     for(int i=0;i<n;i++){
+//         printf("%d ",*(arr+i));
+//     }
+// }
+
+
+//practing insertion sort
+
 #include <stdio.h>
 void insertion(int arr[],int n){
+    int curr;
     for(int i=1;i<n;i++){
         int curr=arr[i];
         int prev=i-1;
@@ -15,11 +41,12 @@ void insertion(int arr[],int n){
     }
 }
 
-void main(){
-    int arr[]={1,3,26,7,6,8,13,4};
-    int n= sizeof(arr)/sizeof(arr[0]);
+int main(){
+    int arr[]={2,13,3,14,5,19,15,6,17,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
     insertion(arr,n);
-    for(int i=0;i<n;i++){
-        printf("%d ",*(arr+i));
+    for(int j=0;j<n;j++){
+        printf("%d ",*(arr+j));
     }
+    return 0;
 }
