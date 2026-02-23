fb7d105e6aaf7562aec79578fea570ac7f77b1f3
kartikDevS <kartik9136@gmail.com>
2026-02-23 19:44:19 +0530

    practicing selectionSort

--- selection_sort.c
+++ selection_sort.c
@@ -3,26 +3,53 @@
 // smallest element in from the array then u place it at the 0 index then you search for the smallest element 
 // in the array from 1 to n , and similarly swap that smallest with the 1 index and so on this goes on
 
+// #include <stdio.h>
+// void selection(int arr[], int n){
+//     for(int i=0;i<n-1;i++){  
+//         int idx=i;    // u dont check for the last value as it would already be sorted 
+//         for(int j=i+1;j<n;j++){
+//             if(arr[j]<arr[idx]){
+//             idx=j;
+//             }
+//         }
+//         int temp=arr[i];
+//         arr[i]=arr[idx];
+//         arr[idx]=temp;
+//     }
+// }                                          // time complexity O(n^2)
+// void main(){
+//     int arr[]={3,9,1,13,14,5,2,8,15};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     selection(arr,n);
+//     for(int a=0;a<n;a++){
+//         printf("%d ",arr[a]);
+//     }
+// }
+
+
+//practicing code
+
 #include <stdio.h>
-void selection(int arr[], int n){
-    for(int i=0;i<n-1;i++){  
-        int idx=i;    // u dont check for the last value as it would already be sorted 
+void selection(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int idx=i;
         for(int j=i+1;j<n;j++){
             if(arr[j]<arr[idx]){
-            idx=j;
+                idx=j;
             }
         }
         int temp=arr[i];
         arr[i]=arr[idx];
         arr[idx]=temp;
     }
-}                                          // time complexity O(n^2)
-void main(){
-    int arr[]={3,9,1,13,14,5,2,8,15};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    selection(arr,n);
-    for(int a=0;a<n;a++){
-        printf("%d ",arr[a]);
-    }
 }
 
+int main(){
+    int arr[]={2,13,3,14,5,19,15,6,17,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    selection(arr,n);
+    for(int j=0;j<n;j++){
+        printf("%d ",*(arr+j));
+    }
+    return 0;
+}
