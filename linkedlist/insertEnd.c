b0a65ab26ebeee648ef65535953fcb5ded372bd3
kartikDevS <kartik9136@gmail.com>
2026-03-06 18:45:07 +0530

    optimal version of adding node at the end

--- linkedlist/insertEnd.c
+++ linkedlist/insertEnd.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//this is the optimal version i.e it has time complexity O(1) as we made the code in
+//a way that it always points to the last node whenever the add_at_end function is called
+
 struct Node{
     int data;
     struct Node*link;
 };
 
-void add_at_end(struct Node*head,int data){
-    struct Node* ptr,*temp;
-    ptr=head;
-    temp=malloc(sizeof(struct Node));
+struct Node* add_at_end(struct Node*ptr,int data){
+    struct Node*temp=malloc(sizeof(struct Node));
     temp->data=data;
     temp->link=NULL;
 
-    while(ptr->link=NULL){ 
-        ptr=ptr->link;
-    }
     ptr->link=temp;
+    return temp;
 }
 
 int main(){
+    struct Node*head;
+    head=malloc(sizeof(struct Node));
+    head->data=69;
+    head->link=NULL;
 
-    add_at_end(head,65);
+    struct Node*ptr;
+    ptr=head;
+
+    ptr=add_at_end(ptr,65);
+    ptr=add_at_end(ptr,89);
+    ptr=add_at_end(ptr,99);
+
+    ptr=head;
+    while(ptr!=NULL){
+        printf("%d ",ptr->data);
+        ptr=ptr->link;
+    }
 
     return 0;
 }
