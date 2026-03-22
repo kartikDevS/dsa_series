eca3baed6af86c2b011e3cd955c45072497d3d14
kartikDevS <kartik9136@gmail.com>
2026-03-22 17:50:39 +0530

    reversing a number stored in linkedlist

---
+++ linkedlist/applications/reverseNumInLL.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node*next;
+};
+typedef struct node* NODE;
+
+NODE create(int x){
+    NODE temp=malloc(sizeof(struct node));
+    temp->data=x;
+    temp->next=NULL;
+    return temp;
+}
+
+NODE store(NODE head,int n){
+    
+    while(n!=0){
+        if(head==NULL){
+            head=create(n%10);
+        }
+        else{
+            NODE temp=create(n%10);
+            temp->next=head;
+            head=temp;
+        }
+        n=n/10;
+    }
+    return head;
+}
+
+NODE reverse(NODE head){
+    NODE current=NULL;
+    NODE Next=head->next;
+    head->next=NULL;
+    while(Next!=NULL){
+        current=Next;
+        Next=Next->next;
+        current->next=head;
+        head=current;
+    }
+    return head;
+}
+
+void print(NODE head){
+    NODE ptr=head;
+    while(ptr!=NULL){
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+
+int main(){
+    NODE head=NULL;
+    int n;
+    printf("Enter the number u want to store in linked list\n");
+    scanf("%d",&n);
+    head=store(head,n);
+    printf("Original Number: ");
+    print(head);
+    head=reverse(head);
+    printf("Reversed Number: ");
+    print(head);
+}
